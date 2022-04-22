043d77fa1a7dfd722b2d26b4559334a12fb0c950
Diego Henrique <diegohrp20@gmail.com>
2022-04-21 21:03:23 -0300

    A good practice in Angular everything that doesn't refer to the design and behavior of a component is good to separate into a service.

--- front-Angular/src/app/app.component.ts
+++ front-Angular/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CarroService } from './carro.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'front-Angular';
+
+  constructor(private carroService: CarroService){
+
+  }
+
 }
